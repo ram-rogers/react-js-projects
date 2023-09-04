b7b259b2f1651ea55c9ec4e06fba1090d209c35e
ram-rogers <ram23rogers@gmail.com>
2023-09-04 22:00:23 +0530

    react-quiz => added responsive

--- react-quiz/src/Components/StartScreen.js
+++ react-quiz/src/Components/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({ numQuestions, dispatch }) {
         <div className="start">
             <h2>Welcome to the React Quiz!</h2>
             <h3>{numQuestions} question to test your React Mastery</h3>
-            <button className="btn btn-ui" onClick={() => dispatch({ type: 'start' })}>Let's Start</button>
+            <button className="btn btn-ui btn-start" onClick={() => dispatch({ type: 'start' })}>Let's Start</button>
         </div>
     )
 }
